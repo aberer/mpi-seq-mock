10f13ed4a1bc2380b42b3b1065ae836058d26c50
Andre Aberer <andre.aberer@h-its.org>
2014-12-09 17:56:02 +0100

    replaced hard coded paths

--- src/mpi.c
+++ src/mpi.c
@@ -1,6 +1,47 @@
-#include "mpi.h" 
+#include "mpi.h"
 
-void MPI_Init( int argc, char **argv)
+#include <string.h>
+
+
+int MPI_Barrier(MPI_Comm comm)
+{
+  return 0 ; 
+}
+
+int MPI_Scatterv(void *sendbuf, int *sendcounts, int *displs, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm )
+{
+  memmove( recvbuf, sendbuf, sendcounts[0] * sendtype );
+  return 0;   
+}
+
+int MPI_Gatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int *recvcounts, int *displs, MPI_Datatype recvtype, int root, MPI_Comm comm)
+{
+  memmove(recvbuf, sendbuf, sendcount * sendtype); 
+  return 0; 
+}
+
+int MPI_Comm_size(MPI_Comm comm, int *size)
+{
+  *size = 1; 
+  return 0;  
+}
+
+
+int MPI_Comm_rank(MPI_Comm comm, int *rank)
+{
+  *rank = 0; 
+  return 0; 
+}
+
+  
+int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm)
+{
+  return 0 ; 
+}
+
+
+
+void MPI_Init( int *argc, char ***argv)
 {
 }
 
@@ -9,7 +50,11 @@ void MPI_Finalize()
 }
 
 
-int MPI_Allreduce(void *sendbuf, void *recvbuf, int count,
-                  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
+int MPI_Allreduce(void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
 {
+  if(sendbuf != MPI_IN_PLACE )
+    {
+      memmove(recvbuf, sendbuf, count * datatype);
+    }
+  return 0 ; 
 }
--- src/mpi.h
+++ src/mpi.h
@@ -2,14 +2,36 @@
 #define MPI_H
 
 #define MPI_COMM_WORLD 0
+#define MPI_IN_PLACE 0
+
+#define MPI_INT 4 
+#define MPI_UNSIGNED_LONG 4 
+#define MPI_DOUBLE  8 
+
+
+#define MPI_SUM 0  
+
 
 typedef int MPI_Op;
 typedef int MPI_Comm;
 typedef int MPI_Datatype; 
 
-void MPI_Init( int argc, char **argv);
+void MPI_Init( int *argc, char ***argv);
 void MPI_Finalize(); 
+void MPI_Broadcast();
 
+int MPI_Barrier(MPI_Comm comm);
+
+
+int MPI_Comm_size(MPI_Comm comm, int *size);
+int MPI_Comm_rank(MPI_Comm comm, int *rank); 
+  
+
+int MPI_Scatterv(void *sendbuf, int *sendcounts, int *displs, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm );
+int MPI_Gatherv(void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int *recvcounts, int *displs, MPI_Datatype recvtype, int root, MPI_Comm comm); 
+
+
+int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm); 
 
 
 #endif /* MPI_H */
